f020a2f3b73b453afd465c13a1e978b37a7d233d
Joseph765 <joseph.chauvin102@gmail.com>
2020-05-22 18:03:48 -0500

    Finish data generation

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -1,27 +1,47 @@
 const faker = require('faker');
+const fs = require('file-system');
+const path = require('path');
 
-const data = [];
+const writePath = path.join(__dirname, '/../data2.csv');
+const writeData = fs.createWriteStream(writePath);
+const str = 'id, gameId, title, publisher, reviewScore, reviewCount, ageRating, newPrice, usedPrice, digitalPrice, storeLocation, inStock\n';
+fs.truncate(writePath, 0, () => {
 
-const fillData = () => {
-  for (var i = 1; i <= 100; i+=1) {
-    const gameObj = {
-      id: i,
-      gameId: i,
-      title: faker.fake('{{company.catchPhraseAdjective}} {{company.bsNoun}}'),
-      publisher: faker.fake('{{company.companyName}}'),
-      reviewScore: Math.ceil(Math.random() * 5),
-      reviewCount: Math.ceil(Math.random() * 5000),
-      ageRating: Math.ceil(Math.random() * 6), //E, E10+, T, M, A, RP
-      newPrice: 60,
-      usedPrice: Math.ceil(Math.random() * 55),
-      digitalPrice: Math.floor(Math.random() * (60 - 45 + 1) + 45),
-      storeLocation: faker.fake('{{address.streetAddress}} {{address.streetName}} {{address.city}}, {{address.state}}'),
-      inStock: faker.random.boolean(),
-    };
-    data.push(gameObj);
+});
+writeData.write(str);
+
+function writeTenMillion(writer, encoding, callback) {
+  let i = 5000000;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      const id = 10000000 - i;
+      const gameId = 10000000 - i;
+      const title = `"${faker.fake('{{company.catchPhraseAdjective}} {{company.bsNoun}}')}"`;
+      const publisher = `"${faker.fake('{{company.companyName}}')}"`;
+      const reviewScore = Math.ceil(Math.random() * 5);
+      const reviewCount = Math.ceil(Math.random() * 5000);
+      const ageRating = Math.ceil(Math.random() * 6);
+      const newPrice = 60;
+      const usedPrice = Math.ceil(Math.random() * 55);
+      const digitalPrice = Math.floor(Math.random() * (60 - 45 + 1) + 45);
+      const storeLocation = `"${faker.fake('{{address.streetAddress}} {{address.streetName}} {{address.city}}, {{address.state}}')}"`;
+      const inStock = faker.random.boolean();
+      const data = `${id}, ${gameId}, ${title}, ${publisher}, ${reviewScore}, ${reviewCount}, ${ageRating}, ${newPrice}, ${usedPrice}, ${digitalPrice}, ${storeLocation}, ${inStock}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
   }
-};
+  write();
+}
 
-fillData();
-
-module.exports = data;
+writeTenMillion(writeData, 'utf-8', () => {
+  writeData.end();
+});
